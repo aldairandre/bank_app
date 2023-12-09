c76f4bdc4978dc448bc5f0021738c8fbca335f6c
aldairandre <aldairandre99@gmail.com>
2023-12-09 08:03:22 +0100

    :sparkles: Pt version

---
+++ src/telaInicial/menu.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void menu()
+{
+  puts("\t============================================");
+  puts("\t\t\tBANCO MONAMI");
+  puts("\t============================================");
+  puts("");
+  puts("");
+  puts("\t\t [1] Criar Conta bancaria");
+  puts("");
+  puts("\t\t [2] Listar usuarios");
+  puts("");
+  puts("\t\t [3] Editar Usuarios");
+  puts("");
+  puts("\t\t [4] Operações");
+  puts("");
+  puts("\t\t [5] Movimento");
+  puts("");
+  puts("\t\t [6] Sair");
+  puts("");
+  puts("");
+  puts("");
+  puts("");
+}
